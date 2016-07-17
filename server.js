e8957b090f7566b27d8764a3a7fd3c1893c59d4e
armytank <p.armytank@gmail.com>
2016-07-17 19:31:57 +0000

    wrote server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    var ip = req.connection.remoteAddress;
+    var language = req.acceptedLanguages[0];
+    var browser = getBrowser(req.headers['user-agent']);
+    res.end(JSON.stringify({'ip':ip, 'language':language, 'browser':browser}));
+})
+app.listen(process.env.PORT, process.env.IP);
+
+function getBrowser(list){
+    var ua = list;
+    var browser;
+    if( /firefox/i.test(ua) )
+    browser = 'firefox';
+    else if( /chrome/i.test(ua) )
+      browser = 'chrome';
+    else if( /safari/i.test(ua) )
+      browser = 'safari';
+    else if( /msie/i.test(ua) )
+      browser = 'msie';
+    else
+      browser = 'unknown';
+    return browser;
+}
